7f8ece2cee521001a0cf2932c0117ac72f8a9885
HomenkoMary <61244966+HomenkoMary@users.noreply.github.com>
2020-12-04 14:23:05 +0400

    не закончено еще

---
+++ src/main/java/mary/lesson/three/Apple.java
@@ -0,0 +1,8 @@
+package mary.lesson.three;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        super(1.0f);
+    }
+}
---
+++ src/main/java/mary/lesson/three/Orange.java
@@ -0,0 +1,8 @@
+package mary.lesson.three;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        super(1.5f);
+    }
+}
