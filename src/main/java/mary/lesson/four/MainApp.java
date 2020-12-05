e38879b0ad918cf3de7e563d4fc4bf0ce9310bf2
HomenkoMary <61244966+HomenkoMary@users.noreply.github.com>
2020-12-06 00:20:14 +0400

    lesson four

--- src/main/java/mary/lesson/four/MainApp.java
+++ src/main/java/mary/lesson/four/MainApp.java
@@ -1,5 +1,19 @@
 package mary.lesson.four;
 
+import java.util.*;
+
 public class MainApp {
-    
+
+    public static void main(String args[])
+    {
+        UniqueWords.getWords();
+        System.out.println("===");
+        PhoneBook book = new PhoneBook();
+
+        System.out.println("Укажите имя абонента для поиска:");
+        Scanner console = new Scanner(System.in);
+        String abonentName = console.nextLine();
+        System.out.println(PhoneBook.getRecord(abonentName));
+    }
+
 }
